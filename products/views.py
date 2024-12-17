0ef9175701c0d30b6719d6e7c30a1352223e14a9
Dhruvesh48 <103595215+Dhruvesh48@users.noreply.github.com>
2024-12-17 18:45:01 +0000

    added product page views, template and css

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 # Create your views here.
+def product_page(request):
+    """ A view to show all products"""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'products/product_page.html', context)
