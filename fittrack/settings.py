f5922d4a77d6f3304e555dedbe0a1f8d16da4194
Dhruvesh48 <103595215+Dhruvesh48@users.noreply.github.com>
2024-12-18 18:26:09 +0000

    added basic checkout functionality

--- fittrack/settings.py
+++ fittrack/settings.py
@@ -172,6 +172,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 5
 
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
