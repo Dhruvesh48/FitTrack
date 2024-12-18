fe94aa74dd7930d8a8e203182b1c6d7226ca76fc
Dhruvesh48 <103595215+Dhruvesh48@users.noreply.github.com>
2024-12-18 09:16:07 +0000

    added product detail views and template

--- products/views.py
+++ products/views.py
@@ -12,3 +12,14 @@ def product_page(request):
     }
 
     return render(request, 'products/product_page.html', context)
+
+def product_detail(request, pk):
+    """ A view to show individual product details """
+     
+    product = get_object_or_404(Product, pk=pk)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'products/product_detail.html', context)
