9e29bc791c5a1b76cf2b91b623b818ca333ba7d3
Dhruvesh48 <103595215+Dhruvesh48@users.noreply.github.com>
2024-12-21 15:47:43 +0000

    added comment edit and delete views and template

--- community/views.py
+++ community/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Post, Category, Comment
 from .forms import CommentForm
-
 # Create your views here.
 def community_list(request):
     categories = Category.objects.all()
@@ -19,18 +20,40 @@ def community_list(request):
     }
     return render(request, 'community/community_list.html', context)
 
+@login_required
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     comments = post.comments.all()
     comment_form = CommentForm()
 
     if request.method == "POST":
-        comment_form = CommentForm(request.POST)
-        if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.author = request.user
-            comment.save()
+        if 'add_comment' in request.POST:
+            # Handle adding a comment
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                messages.success(request, "Your comment has been added.")
+                return redirect('post_detail', post_id=post.id)
+
+        elif 'edit_comment' in request.POST:
+            # Handle editing a comment
+            comment_id = request.POST.get('comment_id')
+            comment = get_object_or_404(Comment, id=comment_id, author=request.user)
+            edit_form = CommentForm(request.POST, instance=comment)
+            if edit_form.is_valid():
+                edit_form.save()
+                messages.success(request, "Your comment has been updated.")
+                return redirect('post_detail', post_id=post.id)
+
+        elif 'delete_comment' in request.POST:
+            # Handle deleting a comment
+            comment_id = request.POST.get('comment_id')
+            comment = get_object_or_404(Comment, id=comment_id, author=request.user)
+            comment.delete()
+            messages.success(request, "Your comment has been deleted.")
             return redirect('post_detail', post_id=post.id)
 
     context = {
@@ -39,3 +62,4 @@ def post_detail(request, post_id):
         'comment_form': comment_form,
     }
     return render(request, 'community/post_detail.html', context)
+
