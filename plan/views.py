774b056e784128b672260a403ad6aca56d40139b
Dhruvesh Prabhakar <103595215+Dhruvesh48@users.noreply.github.com>
2025-02-21 02:59:48 +0000

    updated plans page

--- plan/views.py
+++ plan/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Plan, UserSubscription, ExercisePlan
 
+# Plan List View (for non-subscribed users)
 def plan_list(request):
     plan = Plan.objects.all()
     context = {
@@ -11,23 +12,26 @@ def plan_list(request):
 
 def exercise_plan_list(request):
     if not request.user.is_authenticated:
-        return redirect('account_login')
-    
+        # If the user is not logged in, pass a flag to show the login message
+        context = {
+            'is_logged_in': False,
+        }
+        return render(request, 'plan/exercise_plan_list.html', context)
+
+    # If the user is authenticated, proceed with fetching the plans
     user_subscription = UserSubscription.objects.filter(user=request.user, active=True).first()
 
     if not user_subscription:
         return redirect('plan_list')
-    
+
     exercise_plans = ExercisePlan.objects.all()
     max_plans = 4 if user_subscription.plan.duration == 'monthly' else 1
 
     context = {
         'exercise_plans': exercise_plans,
         'max_plans': max_plans,
+        'is_logged_in': True,
     }
-    
+
     return render(request, 'plan/exercise_plan_list.html', context)
-
-
-
 
